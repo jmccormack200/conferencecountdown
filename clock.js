7e0cb09489325a0503219484a0f65b9a2ea0cd1c
John McCormack <mccormack.wgsi@gmail.com>
2015-08-30 16:21:33 -0400

    Saving before attempting to make dynamic

--- clock.js
+++ clock.js
@@ -1,14 +1,24 @@
-function countdown(year, month, day, display){
-	var oneDay = 24*60*60*1000; 
-	var secondDate = new Date(year,month-1,day);
-	var firstDate = new Date();
-	
-	display.textContent =  Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-	setInterval(countdown, 1000000);
+function conference(name, year, month, day, display, image){
+	this.name = name;
+	this.year = year;
+	this.month = month;
+	this.day = day;
+	this.display = display;
+	this.image = image;
+
+	this.countdown = function(){
+		var oneDay = 24*60*60*1000; 
+		var secondDate = new Date(this.year, this.month - 1, this.day);
+		var firstDate = new Date();
+		
+		this.display.textContent =  Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+		setInterval(this.countdown, 1000000);
+	}
 }
 
 window.onload = function(){
-	display = document.querySelector('#time');
-	countdown(2015,11,1,display);
+	display = document.querySelector('#time1');
+	var conf = new conference("Augmented Human",2015,11,1,display);
+	conf.countdown();
 };
 
