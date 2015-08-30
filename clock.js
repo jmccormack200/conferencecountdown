11e2c48b2ca325958777c570008da0dc796f5b3d
John McCormack <mccormack.wgsi@gmail.com>
2015-08-30 11:30:22 -0400

    Have the date working, but its ugly and not scaleable

---
+++ clock.js
@@ -0,0 +1,14 @@
+function countdown(year, month, day, display){
+	var oneDay = 24*60*60*1000; 
+	var secondDate = new Date(year,month-1,day);
+	var firstDate = new Date();
+	
+	display.textContent =  Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+	setInterval(countdown, 1000000);
+}
+
+window.onload = function(){
+	display = document.querySelector('#time');
+	countdown(2015,9,1,display);
+};
+
